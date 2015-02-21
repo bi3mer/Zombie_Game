0fd4b9aec1aa7a7fff056a1fcd7dfb510ce527a1
Mikeman3561 <Mikeman3561@gmail.com>
2015-02-21 13:56:31 -0500

    Merge branch 'master' of https://github.com/bi3mer/Zombie_Game
    Conflicts:
    	Assembly-CSharp-vs.csproj
    	Assembly-CSharp.csproj
    	Assets/Work Flow Scenes/codyScene.unity
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/assetDatabase3
    	Library/guidmapper
    	Library/metadata/80/80a3616ca19596e4da0f10f14d241e9f
    	Library/metadata/b4/b4dafd153cb44d64298435c51563e6a4
    	Library/metadata/f5/f5f67c52d1564df4a8936ccd202a3bd8
    	Zombie_Game-csharp.sln
    	Zombie_Game.sln

---
+++ Assets/Scripts/GUI/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	public string scene = "gameScene";
+
+	public void StartGame(string s)
+	{
+		if (s == null)
+			s = scene;
+		Application.LoadLevel(s);
+	}
+}
