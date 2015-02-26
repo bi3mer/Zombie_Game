c929c307cd49d76c90b31cc3d111b4b7a1545fce
rowlandsc <cr549@drexel.edu>
2015-02-26 12:09:06 -0500

    scene updates
    woohooo

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Tamer/General/lightFlicker")]
+[RequireComponent(typeof(Light))]
+
+public class LightFlicker : MonoBehaviour {
+	
+	public float interval = 0.7F;
+	private Color col;
+	private float flick = 0.0F;
+
+	
+	void Start () {
+		col = light.color;
+
+	}
+	
+		void Update () {
+			if (Random.value >= interval) {
+				flick = Random.value;
+				light.color = new Color(col.r * flick, col.g * flick, col.b * flick);
+
+		}
+	}
+}
