7ac86c3b34d603b285ea3954a3a7dc94f51288be
rowlandsc <cr549@drexel.edu>
2015-03-05 13:12:40 -0500

    all the things jhun did for today
    change player attack for drone abstract

---
+++ Assets/DronePush.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp; 
+public class DronePush : DroneAbstract {
+
+	// Use this for initialization
+	void Start () {
+		searchRange += Random.Range (5, 10);
+		health 		+= Random.Range (0, 50); // increase scale of model based on health?
+		moveSpeed 	+= Random.Range (1, 5);
+
+		// multiply values based on waves for balancing
+		health 		+= (health      * EventHandler.Instance.getWave() / 5);  //magic numbers in here for now. will balance later
+		moveSpeed   += (moveSpeed + (EventHandler.Instance.getWave () / 10));
+		//attack      += (attack 		+ (EventHandler.Instance.getWave()/10));
+		
+		this.GetComponent<NavMeshAgent> ().stoppingDistance = 0;
+		this.GetComponent<NavMeshAgent> ().speed = moveSpeed;
+		this.GetComponent<NavMeshAgent> ().enabled = false;
+		searchRange = 20;
+
+		player = Player.transform;
+
+		HiveMind.Instance.addDrone(this);
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "player")
+		{
+			col.gameObject.rigidbody.AddExplosionForce(50.0f,transform.position,5.0f,3.0f);
+		}
+	}
+
+	
+
+}
