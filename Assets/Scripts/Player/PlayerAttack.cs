823738facfaa2c38108de5055cd995e687d66f5b
rowlandsc <cr549@drexel.edu>
2015-03-14 16:21:17 -0400

    COlan Sukz
    Conflicts:
    	Library/InspectorExpandedItems.asset
    	Library/assetDatabase3
    	Library/guidmapper

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -37,10 +37,9 @@ public class PlayerAttack : MonoBehaviour {
 
 	// If the arms collide with something than deal damage and play the sounds effect.
 	void OnCollisionEnter(Collision collider){
-<<<<<<< HEAD
+
 		print(collider.gameObject.name);
-=======
->>>>>>> origin/master
+
 		if(collider.gameObject.CompareTag("enemy")){
 			GameObject enemy = collider.gameObject;
 			DealDamage(enemy);
